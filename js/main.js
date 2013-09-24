021110a3b8288715432edabcbaded47f84d057e2
Austin-Xie <xie_weiyu@yeah.net>
2013-09-24 19:32:44 +1000

    - Add nivo-slider feature - Update CSS for position

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ requirejs.config({
         app: '../app',
         underscore: 'underscore/underscore',
         jquery: 'jquery/jquery-2.0.3',
-        'jquery-ui': 'jquery-ui/jquery-ui.min'
+        'jquery-ui': 'jquery-ui/jquery-ui.min',
+        'nivo-slider': 'nivo-slider/jquery.nivo.slider.pack'
     },
 
     shim: {
@@ -31,12 +32,15 @@ requirejs.config({
         'jquery-ui': {
             deps: ['jquery'],
             exports: 'jquery-ui'
+        },
+        'nivo-slider': {
+            deps: ['jquery']
         }
     }
 });
 
 // Start the main app logic.
-requirejs(['underscore', 'jquery', 'jquery-ui'],
+requirejs(['underscore', 'jquery',  'nivo-slider', 'jquery-ui'],
     function   (_, $) {
         console.info("patience!");
         _.each([1,2,3], function(v) {
@@ -44,5 +48,32 @@ requirejs(['underscore', 'jquery', 'jquery-ui'],
         });
 
         $( "#accordion" ).accordion();
+
+        $('#slider').nivoSlider({
+            effect: 'random',               // Specify sets like: 'fold,fade,sliceDown'
+            slices: 15,                     // For slice animations
+            boxCols: 8,                     // For box animations
+            boxRows: 1,                     // For box animations
+            animSpeed: 500,                 // Slide transition speed
+            pauseTime: 3000,                // How long each slide will show
+            startSlide: 0,                  // Set starting Slide (0 index)
+            directionNav: true,             // Next & Prev navigation
+            controlNav: true,               // 1,2,3... navigation
+            controlNavThumbs: false,        // Use thumbnails for Control Nav
+            pauseOnHover: true,             // Stop animation while hovering
+            manualAdvance: false,           // Force manual transitions
+            prevText: 'Prev',               // Prev directionNav text
+            nextText: 'Next',               // Next directionNav text
+            randomStart: false,             // Start on a random slide
+            beforeChange: function(){},     // Triggers before a slide transition
+            afterChange: function(){},      // Triggers after a slide transition
+            slideshowEnd: function(){},     // Triggers after all slides have been shown
+            lastSlide: function(){},        // Triggers when last slide is shown
+            afterLoad: function(){}         // Triggers when slider has loaded
+        });
+
+        $(window).load(function() {
+            $('#slider').nivoSlider();
+        });
     }
 );
