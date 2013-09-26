80bbc70238d9b51e13ce01b0fbb093574d2d8b27
Austin-Xie <xie_weiyu@yeah.net>
2013-09-27 01:07:37 +1000

    - set correct  el to enable event mapping

--- js/app/views/AlbumBrowseView.js
+++ js/app/views/AlbumBrowseView.js
@@ -7,18 +7,18 @@
  */
 
 define(
-    ['backbone',, 'jquery', 'underscore',  'jquery-ui'],
+    ['backbone', 'jquery', 'underscore',  'jquery-ui'],
     function(Backbone, $, _) {
         "use strict";
 
-        return Backbone.View.extend({
+        var BrowseView =  Backbone.View.extend({
 
-            tagName: "div.browse_panel",
+            el: $(".tab_header"),
 
             className: "AlbumBrowseView",
 
             events: {
-                "click #radio1": "browse",
+                'click #radio1, #radio2, #radio3': 'browse'
 //                "click .button.edit":   "openEditDialog",
 //                "click .button.delete": "destroy"
             },
@@ -26,17 +26,22 @@ define(
             initialize: function() {
 //                this.listenTo(this.model, "change", this.render);
                 console.log("initialize called");
+                $( "#radio" ).buttonset();
+                _.bindAll(this,'render');
             },
 
             render: function() {
                 console.log("render called");
-
             },
 
-            browse: function () {
-                console.log("radio1 clicked");
+            browse: function (event) {
+                event.preventDefault();
+//                console.log("radio1 clicked");
+                alert('clicked');
             }
 
         });
+
+        return BrowseView;
     }
 );
