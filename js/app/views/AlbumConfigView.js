32648561675a1e4c05a7397df6c9f66c31778112
Austin-Xie <xie_weiyu@yeah.net>
2013-09-28 17:46:01 +1000

    - Add Config view

---
+++ js/app/views/AlbumConfigView.js
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-9-28
+ * Time: 下午5:25
+ * To change this template use File | Settings | File Templates.
+ */
+
+define(
+	[
+		'Backbone',
+		'jquery',
+		'underscore'
+	],
+	function(Backbone, $, _) {
+
+	}
+);
